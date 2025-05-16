7ecdff194fffb1e1eb08bbb138208df825f0acdc
Orion <187824312+thementat42@users.noreply.github.com>
2025-05-16 16:27:17 -0400

    update macros

--- src/global_macros.h
+++ src/global_macros.h
@@ -3,9 +3,16 @@
 
 #define DEBUG 1
 
-
+#ifdef __cplusplus
+#define EXT_C_BEGIN extern "C" {
+#define EXT_C_END }
 #define MANG_BEGIN namespace Manganese {
 #define MANG_END }
+#else  // ^^ __cplusplus vv !__cplusplus
+#define EXT_C_BEGIN
+#define EXT_C_END
+#define MANG_BEGIN
+#define MANG_END
+#endif  // __cplusplus
 
-
-#endif // GLOBAL_MACROS_H
+#endif  // GLOBAL_MACROS_H
