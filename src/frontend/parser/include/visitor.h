98932a8b7da8ee713c6f56ea7b6ff5dc67503300
Orion <187824312+thementat42@users.noreply.github.com>
2025-05-19 19:58:26 -0400

    add initial visitor template

---
+++ src/frontend/parser/include/visitor.h
@@ -0,0 +1,15 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+
+#include "../../../global_macros.h"
+
+MANG_BEGIN
+namespace parser {
+
+class Visitor {
+};
+
+}  // namespace parser
+MANG_END
+
+#endif // VISITOR_H
