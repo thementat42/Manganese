92afb2e88845b9a12797e979a040da1466237ece
Orion <187824312+thementat42@users.noreply.github.com>
2025-05-19 19:58:17 -0400

    create parser_tests.cpp

---
+++ tests/parser_tests.cpp
@@ -0,0 +1,12 @@
+#include "testrunner.h"
+
+#include "../src/frontend/parser/include/parser.h"
+#include "../src/global_macros.h"
+
+MANG_BEGIN
+namespace tests {
+int runParserTests(TestRunner& runner) {
+    return 0;
+}
+}  // namespace tests
+MANG_END
