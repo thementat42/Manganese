f6b70ba409954e421da4e8ac083eb766985e9e7c
Orion <187824312+thementat42@users.noreply.github.com>
2025-05-30 22:57:24 -0400

    added assertion macros

--- src/global_macros.h
+++ src/global_macros.h
@@ -5,8 +5,22 @@
 
 #define DEBUG 1
 
+#define ASSERT_CRITICAL(condition, message) \
+    do { \
+        if (!(condition)) { \
+            fprintf(stderr, "\033[31mCritical assertion failed: %s\n\033[0m", message); \
+            abort(); \
+        } \
+    } while (0)
+
 #if DEBUG
 #define DEBUG_FUNC  // In debug mode, leave functions as is
+#define ASSERT_DEBUG(condition, message) \
+    do { \
+        if (!(condition)) { \
+            fprintf(stderr, "033[33mDebug assertion failed: %s\n\033[0m", message); \
+        } \
+    } while (0)
 #else               // ^ DEBUG ^ | v !DEBUG v
 // In release mode, inline the debug functions (since they do nothing)
 #ifdef _MSC_VER
@@ -15,6 +29,7 @@
 // GCC/Clang
 #define DEBUG_FUNC inline __attribute__((always_inline))
 #endif  // _MSC_VER
+#define ASSERT_DEBUG(condition, message)  // no-op in release mode
 #endif  // DEBUG
 
 #ifdef __cplusplus
