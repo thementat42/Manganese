d83346884c2c2efb74ae94ca4a3771012099d022
Orion <187824312+thementat42@users.noreply.github.com>
2025-06-01 18:47:14 -0400

    refactor: improve formatting and consistency of assertion macros

--- src/global_macros.h
+++ src/global_macros.h
@@ -5,32 +5,37 @@
 
 #define DEBUG 1
 
-#define ASSERT_CRITICAL(condition, message) \
-    do { \
-        if (!(condition)) { \
+#define ASSERT_CRITICAL(condition, message)                                             \
+    do {                                                                                \
+        if (!(condition)) {                                                             \
             fprintf(stderr, "\033[31mCritical assertion failed: %s\n\033[0m", message); \
-            abort(); \
-        } \
+            abort();                                                                    \
+        }                                                                               \
     } while (0)
 
 #if DEBUG
 #define DEBUG_FUNC  // In debug mode, leave functions as is
-#define ASSERT_DEBUG(condition, message) \
-    do { \
-        if (!(condition)) { \
-            fprintf(stderr, "033[33mDebug assertion failed: %s\n\033[0m", message); \
-        } \
+#define LOG_VAR(var)                                                                        \
+    do {                                                                                    \
+        std::cout << "\033[34mDEBUG: " << #var << " = " << (var) << "\033[0m" << std::endl; \
     } while (0)
-#else               // ^ DEBUG ^ | v !DEBUG v
+#define ASSERT_DEBUG(condition, message)                                            \
+    do {                                                                            \
+        if (!(condition)) {                                                         \
+            fprintf(stderr, "033[33mDebug assertion failed: %s\n\033[0m", message); \
+        }                                                                           \
+    } while (0)
+#else  // ^ DEBUG ^ | v !DEBUG v
 // In release mode, inline the debug functions (since they do nothing)
 #ifdef _MSC_VER
 #define DEBUG_FUNC inline __forceinline
 #else  // ^ _MSC_VER ^ | v ! _MSC_VER v
 // GCC/Clang
 #define DEBUG_FUNC inline __attribute__((always_inline))
-#endif  // _MSC_VER
+#endif                                    // _MSC_VER
 #define ASSERT_DEBUG(condition, message)  // no-op in release mode
-#endif  // DEBUG
+#define LOG_VAR(var)                      // no-op in release mode
+#endif                                    // DEBUG
 
 #ifdef __cplusplus
 #define EXT_C_BEGIN extern "C" {
