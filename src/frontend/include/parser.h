4d8af26aa354c541f984d5cc33f06c462166db06
Orion <187824312+thementat42@users.noreply.github.com>
2025-06-03 22:18:47 -0400

    refactor parser, start parsing for literals

--- src/frontend/include/parser.h
+++ src/frontend/include/parser.h
@@ -1,35 +1,88 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <functional>
 #include <memory>
-#include <optional>
+#include <unordered_map>
+#include <vector>
 
+#include "../../core/include/keywords.h"
+#include "../../core/include/operators.h"
+#include "../../core/include/token.h"
 #include "../../global_macros.h"
-#include "lexer.h"
 #include "ast.h"
+#include "lexer.h"
 
-MANG_BEGIN
+namespace manganese {
 namespace parser {
-constexpr auto NONE = std::nullopt;
-using core::Token;
-using str = std::string;
+using core::TokenType,
+    core::OperatorBindingPower,
+    core::Token;
+
 class Parser {
    private:  // private variables
+    bool hasError = false;
     std::unique_ptr<lexer::Lexer> lexer;
-    std::vector<Token> tokenCache;  // store tokens fromthe current cache
+    std::vector<core::Token> tokenCache;  // store relevant tokens for the current parsing context
 
-   private:  // private functions
-    bool doneParsing();
-    // ast::StatementPtr parseStatement();
+    using statementHandler_t = std::function<ast::StatementPtr(Parser*)>;
+    using nullDenotationHandler_t = std::function<ast::ExpressionPtr(Parser*)>;
+    using leftDenotationHandler_t = std::function<ast::ExpressionPtr(Parser*, ast::ExpressionPtr, core::OperatorBindingPower)>;
 
-   public:   // public functions
-    Parser(const str& source, const lexer::Mode mode);
-    ~Parser() = default;
+   public:   // public variables
+   private:  // private methods
+    ast::StatementPtr parseStatement();
+    ast::ExpressionPtr nullDenotationHandler();
+    ast::ExpressionPtr leftDenotationHandler(ast::ExpressionPtr left, core::OperatorBindingPower bindingPower);
 
-    // ast::Block parse();
+    //~ Lookup Tables
+    std::unordered_map<core::TokenType, statementHandler_t> statementLookup;
+
+    /**
+     * @brief Null denotation: we don't expect anything to the left of the token
+     */
+    std::unordered_map<core::TokenType, nullDenotationHandler_t> nullDenotationLookup;
+
+    /**
+     * @brief Left denotation: we expect an expression to the left of the token
+     */
+    std::unordered_map<core::TokenType, leftDenotationHandler_t> leftDenotationLookup;
+    std::unordered_map<core::TokenType, core::OperatorBindingPower> bindingPowerLookup;
+
+    // TODO: Inline these functions into the constructor later (removing the definitions)
+    inline void led(core::TokenType type, core::OperatorBindingPower bindingPower,
+                    leftDenotationHandler_t handler) {
+        leftDenotationLookup[type] = handler;
+        bindingPowerLookup[type] = bindingPower;
+    }
+
+    inline void nud(core::TokenType type,
+                    nullDenotationHandler_t handler) {
+        nullDenotationLookup[type] = handler;
+        bindingPowerLookup[type] = core::OperatorBindingPower::Primary;  // Have to bind as tightly as possible since there's nothing else to bind to
+    }
+
+    inline void stmt(core::TokenType type, statementHandler_t handler) {
+        statementLookup[type] = handler;
+        bindingPowerLookup[type] = core::OperatorBindingPower::Default;
+    }
+
+    inline void initializeLookups();
+
+    //~ Wrappers around the lexer
+    inline lexer::Token peekToken(size_t offset = 0) { return lexer->peekToken(offset); };
+    [[nodiscard]] inline lexer::Token consumeToken() { return lexer->consumeToken(); };
+
+    bool done() { return peekToken().getType() == core::TokenType::EndOfFile; }
+
+   public:  // public methods
+    Parser() = default;
+    Parser(const std::string& source, lexer::Mode mode);
+    ~Parser();
+
+    ast::Block parse();
 };
-
 }  // namespace parser
-MANG_END
+}  // namespace manganese
 
 #endif  // PARSER_H
--- src/frontend/parser.cpp
+++ src/frontend/parser.cpp
@@ -1,20 +1,48 @@
 #include "include/parser.h"
 
-#include "../core/include/token.h"
+#include <stdio.h>
+
 #include "../global_macros.h"
 #include "include/ast.h"
-#include "include/lexer.h"
 
-MANG_BEGIN
+//! For generics, might have to do two passes
+//! First pass: just identify what the function/bundle/blueprint names are and keep those in a table
+//! Then re-parse the file using that table to handle the "less-than vs generic" ambiguity
+//! On the second pass, any time you see a function/bundle/blueprint name followed by a left angle bracket (<), treat it as a generic type (it's invalid to compare a function/bundle/blueprint name with something else)
+
+namespace manganese {
 namespace parser {
 
-Parser::Parser(const str& source, const lexer::Mode mode) {
-    this->lexer = std::make_unique<lexer::Lexer>(source, mode);
+inline void Parser::initializeLookups(){
+    // TODO: Update this to account for different sizes
+    nud(core::TokenType::IntegerLiteral, [](Parser* parser) {
+        double value = std::stod(parser->consumeToken().getLexeme()); 
+        return std::make_unique<ast::NumberExpression>(value);
+    });
+    nud(core::TokenType::FloatLiteral, [](Parser* parser) {
+        std::string lexeme = parser->consumeToken().getLexeme();
+        if (lexeme.back() == 'f' || lexeme.back() == 'F') {
+                return std::make_unique<ast::NumberExpression>(std::stof(lexeme));
+        } else {
+            return std::make_unique<ast::NumberExpression>(std::stod(lexeme));
+        }
+    });
+    nud(core::TokenType::StrLiteral, [](Parser* parser) { 
+        std::string value = parser->consumeToken().getLexeme(); 
+        return std::make_unique<ast::StringExpression>(value);
+    });
 }
 
-inline bool Parser::doneParsing() {
-    return lexer->peekToken().getType() != core::TokenType::EndOfFile;
+Parser::Parser(const std::string& source, lexer::Mode mode) : lexer(std::make_unique<lexer::Lexer>(source, mode)) {
+    initializeLookups();
 }
 
+// ast::Block Parser::parse() {
+//     ast::Block body;
+//     while (!done()) {
+//         body.push_back(parseStatement());
+//     }
+//     return body;
+// }
 }  // namespace parser
-MANG_END
+}  // namespace manganese
