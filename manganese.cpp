71562c34755c37e0560fe7ba3580e0d1f7a360b2
Orion <187824312+thementat42@users.noreply.github.com>
2025-06-07 14:49:49 -0400

    change loop index type to int to avoid comparison errors

--- manganese.cpp
+++ manganese.cpp
@@ -1,15 +1,11 @@
 #include "src/global_macros.h"
 
-#undef DEBUG
-#define DEBUG 0  // Use this for release builds
-
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
     std::cout << "Hello, World!" << '\n';
-    for (size_t i = 1; i < argc; i++) {
+    for (int i = 1; i < argc; i++) {
         std::cout << "Arg" << i << ": " << argv[i] << '\n';
     }
-
     return 0;
 }
