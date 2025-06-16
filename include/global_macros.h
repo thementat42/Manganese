3eee2b6daf1f9adc1b4a7ca9f889843b00be71a1
Rahi <187824312+thementat42@users.noreply.github.com>
2025-06-16 14:10:49 -0400

    remove macros defined in compiler_config.h

--- include/global_macros.h
+++ include/global_macros.h
@@ -7,49 +7,12 @@
 #ifndef GLOBAL_MACROS_H
 #define GLOBAL_MACROS_H
 
+#include <utils/compiler_config.h>
+
 #include <stdint.h>
 
 #include <iostream>
 
-#ifndef DEBUG    // Defined by CMake (see CMakeLists.txt)
-#define DEBUG 0  // Default to release mode if not defined
-#endif           // DEBUG
-
-#define DISCARD(x) (void)(x)  // Explicitly discard a value
-
-#if defined(__clang__)
-#define __DISABLE_CONVERSION_WARNING \
-    _Pragma("clang diagnostic push") \
-        _Pragma("clang diagnostic ignored \"-Wconversion\"")
-
-#define __ENABLE_CONVERSION_WARNING \
-    _Pragma("clang diagnostic pop")
-
-#elif defined(__GNUC__)
-#define __DISABLE_CONVERSION_WARNING \
-    _Pragma("GCC diagnostic push")   \
-        _Pragma("GCC diagnostic ignored \"-Wconversion\"")
-
-#define __ENABLE_CONVERSION_WARNING \
-    _Pragma("GCC diagnostic pop")
-
-#elif defined(_MSC_VER)
-#define __DISABLE_CONVERSION_WARNING \
-    __pragma(warning(push))          \
-        __pragma(warning(disable : 4244))  // 4244 is the warning code for conversion from 'type1' to 'type2', possible loss of data
-
-#define __ENABLE_CONVERSION_WARNING \
-    __pragma(warning(pop))
-
-#else
-#define __DISABLE_CONVERSION_WARNING
-#define __ENABLE_CONVERSION_WARNING
-#endif
-
-#define DISABLE_CONVERSION_WARNING __DISABLE_CONVERSION_WARNING  // Supress a warning about a conversion causing data loss
-#define ENABLE_CONVERSION_WARNING __ENABLE_CONVERSION_WARNING    // Re-enable the conversion warning
-
-#ifdef __cplusplus
 #define EXT_C_BEGIN extern "C" {
 #define EXT_C_END }
 #define MANGANESE_BEGIN namespace Manganese {
@@ -64,7 +27,6 @@
 #define DISABLE_COPY_MOVE(ClassName)   \
     DISABLE_COPY_AND_ASSIGN(ClassName) \
     DISABLE_MOVE(ClassName)  // Disable both copy and move semantics
-#endif                       // __cplusplus
 
 #define ASSERT_CRITICAL(condition, message)                                               \
     do {                                                                                  \
