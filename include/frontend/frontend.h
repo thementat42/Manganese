22ad31ede16596287dea3fa3bcf17d26f4a5c7f0
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-07-07 14:31:41 -0400

    add central frontend file for future use

---
+++ include/frontend/frontend.h
@@ -0,0 +1,14 @@
+/**
+ * @file frontend.h
+ * @brief Central header for all frontend components
+ */
+
+#ifndef MANGANESE_INCLUDE_FRONTEND_FRONTEND_H
+#define MANGANESE_INCLUDE_FRONTEND_FRONTEND_H
+
+#include "ast.h"
+#include "lexer.h"
+#include "parser.h"
+#include "visitor.h"
+
+#endif  // MANGANESE_INCLUDE_FRONTEND_FRONTEND_H
