1bf9792f470d79551f228535238915ddfab19e89
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-07-16 17:32:09 -0400

    add central header for semantic analysis components

---
+++ include/frontend/semantic.h
@@ -0,0 +1,11 @@
+/**
+ * @file semantic.h
+ * @brief Central header for the semantic analysis components
+ */
+
+#ifndef MANGANESE_INCLUDE_FRONTEND_SEMANTIC_H
+#define MANGANESE_INCLUDE_FRONTEND_SEMANTIC_H
+
+#include <frontend/semantic/symbol_table.h>
+
+#endif  // MANGANESE_INCLUDE_FRONTEND_SEMANTIC_H
