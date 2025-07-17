8a9a7785f309f0d8ce77714c20f82c8b2ce6226b
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-07-16 21:40:39 -0400

    add semantic analyzer.h as an include

--- include/frontend/semantic.h
+++ include/frontend/semantic.h
@@ -7,5 +7,6 @@
 #define MANGANESE_INCLUDE_FRONTEND_SEMANTIC_H
 
 #include <frontend/semantic/symbol_table.h>
+#include <frontend/semantic/semantic_analyzer.h>
 
 #endif  // MANGANESE_INCLUDE_FRONTEND_SEMANTIC_H
