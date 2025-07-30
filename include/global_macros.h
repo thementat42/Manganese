6b81521494de7d73a12add0dac063924aa659cbb
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-07-30 19:21:20 -0400

    fix NOT_IMPLEMENTED macro (added appropriate line continuation characters)

--- include/global_macros.h
+++ include/global_macros.h
@@ -55,7 +55,6 @@
 #define __UNREACHABLE(message) COMPILER_UNREACHABLE
 #endif  // DEBUG
 
-
 /**
  * Indicates a place that should never be reached
  * In debug mode, throws an std::runtime_error
@@ -80,13 +79,13 @@
 #define DISCARD(value) (void)(value)  // Explicitly discard a value
 
 #if DEBUG
-#define __NOT_IMPLEMENTED \
-do {
-    std::cerr << "\033[31m" << __func__ << "is not implemented yet!";
-    std::cerr << " (if this occurred in a test run, make sure to implement the function, then re-run the tests)\n\033[0m";
-    PRINT_LOCATION;
-    throw std::runtime_error("Not implemented yet");
-} while (0)
+#define __NOT_IMPLEMENTED                                                                                                      \
+    do {                                                                                                                       \
+        std::cerr << "\033[31m" << __func__ << "is not implemented yet!";                                                      \
+        std::cerr << " (if this occurred in a test run, make sure to implement the function, then re-run the tests)\n\033[0m"; \
+        PRINT_LOCATION;                                                                                                        \
+        throw std::runtime_error("Not implemented yet");                                                                       \
+    } while (0);
 #else  // ^^ DEBUG vv !DEBUG
 #define __NOT_IMPLEMENTED
 #endif  // DEBUG
