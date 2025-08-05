c1cd35ec095b82a607f5544f186b167bcfa30d07
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-08-05 13:14:09 -0400

    add basic checks for repeat loops

--- src/frontend/semantic/statements/semantic_analyzer_loops.cpp
+++ src/frontend/semantic/statements/semantic_analyzer_loops.cpp
@@ -1,13 +1,39 @@
-#include <frontend/semantic/semantic_analyzer.hpp>
 #include <frontend/ast.hpp>
+#include <frontend/semantic/semantic_analyzer.hpp>
+#include "frontend/ast/ast_base.hpp"
+
 
 namespace Manganese {
 
 namespace semantic {
 
 void SemanticAnalyzer::checkRepeatLoopStatement(ast::RepeatLoopStatement* statement) {
-    DISCARD(statement);
-    NOT_IMPLEMENTED;
+    checkExpression(statement->numIterations.get());
+    const ast::Type* iterationType = statement->numIterations->getType();
+
+    if (!ast::isPrimitiveType(iterationType)) {
+        logError("Repeat loop iteration count must be a numeric type, not {}", statement,
+                 ast::toStringOr(iterationType));
+        return;
+    }
+    if (!isAnyInt(iterationType)) {
+        logError("Repeat loops must have an integer type, not {}", statement, ast::toStringOr(iterationType));
+        // if (isFloat(iterationType)) {
+        //     logWarning("Repeat loop iteration count should be an integer, not {}. Value will be rounded up",
+        //     statement,
+        //                ast::toStringOr(iterationType));
+        // ! Round up
+        // } else {
+        //     logError("Repeat loop iteration count must be an integer type, not {}", statement,
+        //              ast::toStringOr(iterationType));
+        //     return;
+        // }
+    }
+    enterScope();
+    ++context.repeatLoop;
+    checkBlock(statement->body);
+    --context.repeatLoop;
+    exitScope();
 }
 
 void SemanticAnalyzer::checkWhileLoopStatement(ast::WhileLoopStatement* statement) {
