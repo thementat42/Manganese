9e55b38eef273f4931a304cc009e0a4d37f07739
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-08-25 15:51:13 -0400

    update macro names

--- include/global_macros.hpp
+++ include/global_macros.hpp
@@ -9,44 +9,27 @@
 #include <stdint.h>
 #include <utils/compiler_config.h>
 
+// Some common includes that are used frequently
 #include <iostream>
 #include <stdexcept>
 #include <utility>
 
-#define __PRINT_LOCATION                                                                                       \
+#define __MANGANESE_PRINT_LOCATION                                                                             \
     std::cerr << "\033[33m In file: " << __FILE__ << ", at line " << __LINE__ << ": when running " << __func__ \
               << "\033[0m\n";
 
 #define PRINT_LOCATION \
-    __PRINT_LOCATION  // Print the location of the log message (in the compiler source, not the user code)
-
-#if __cplusplus >= 202302L
-#define __COMPILER_UNREACHABLE std::unreachable();  // compiler agnostic, but still allows for optimisation
-// If < C++23, use a compiler-specific implementation
-#elif defined(__GNUC__) || defined(__clang__)
-#define __COMPILER_UNREACHABLE __builtin_unreachable();
-#elif defined(_MSC_VER)
-#define __COMPILER_UNREACHABLE __assume(false);
-#else  // If no (known) compiler-specific implementation is available, do nothing
-#define __COMPILER_UNREACHABLE
-#endif  // __cplusplus >= 202302L
-
-/**
- * In pre-C++23 builds, uses a compile-specific unreachable code marker, if available.
- * MSVC, Clang and GCC are supported.
- * In C++23+, uses `std::unreachable()`
- */
-#define COMPILER_UNREACHABLE __COMPILER_UNREACHABLE
+    __MANGANESE_PRINT_LOCATION  // Print the location of the log message (in the compiler source, not the user code)
 
 #if DEBUG
-#define __UNREACHABLE(message)                                                       \
+#define __MANGANESE_ASSERT_UNREACHABLE(message)                                      \
     do {                                                                             \
         std::cerr << "\033[31mUnreachable code reached: " << message << "\n\033[0m"; \
         PRINT_LOCATION;                                                              \
         throw std::runtime_error(message);                                           \
     } while (0);
 #else  // ^^ DEBUG vv !DEBUG
-#define __UNREACHABLE(message) COMPILER_UNREACHABLE
+#define __MANGANESE_ASSERT_UNREACHABLE(message) COMPILER_UNREACHABLE
 #endif  // DEBUG
 
 /**
@@ -55,7 +38,7 @@
  * In release mode, marks the block as unreachable, for optimizations
  * @note see
  */
-#define ASSERT_UNREACHABLE(message) __UNREACHABLE(message)
+#define ASSERT_UNREACHABLE(message) __MANGANESE_ASSERT_UNREACHABLE(message)
 
 #if DEBUG
 #define __noexcept_if_release  // In debug mode, these functions are more likely to throw -- don't use noexcept
@@ -74,7 +57,7 @@
 #define DISCARD(value) (void)(value)  // Explicitly discard a value
 
 #if DEBUG
-#define __NOT_IMPLEMENTED(reason)                                                                                        \
+#define __MANGANESE_NOT_IMPLEMENTED(reason)                                                                              \
     do {                                                                                                                 \
         std::cerr << "\033[31m" << __func__ << "is not implemented yet!";                                                \
         std::cerr << "Reason: " << reason << "\n";                                                                       \
@@ -84,9 +67,9 @@
         throw std::runtime_error("Not implemented yet");                                                                 \
     } while (0);
 #else  // ^^ DEBUG vv !DEBUG
-#define __NOT_IMPLEMENTED(reason)
+#define __MANGANESE_NOT_IMPLEMENTED(reason)
 #endif  // DEBUG
 
-#define NOT_IMPLEMENTED(reason) __NOT_IMPLEMENTED(reason)  // Indicates that a function is not implemented
+#define NOT_IMPLEMENTED(reason) __MANGANESE_NOT_IMPLEMENTED(reason)  // Indicates that a function is not implemented
 
 #endif  // MANGANESE_INCLUDE_GLOBAL_MACROS_HPP
