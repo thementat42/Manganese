b174bc1d398463feb3975ff286ad88efd26dfad0
thementat42 <187824312+thementat42@users.noreply.github.com>
2025-12-27 21:07:56 -0500

    add includes to semantic master header

--- include/frontend/semantic.hpp
+++ include/frontend/semantic.hpp
@@ -6,4 +6,7 @@
 #ifndef MANGANESE_INCLUDE_FRONTEND_SEMANTIC_HPP
 #define MANGANESE_INCLUDE_FRONTEND_SEMANTIC_HPP
 
+#include "semantic/analyzer.hpp"
+#include "semantic/symbol_table.hpp"
+
 #endif  // MANGANESE_INCLUDE_FRONTEND_SEMANTIC_HPP
