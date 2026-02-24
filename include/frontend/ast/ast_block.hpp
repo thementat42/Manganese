8a4ce893b40e0950c2745965c6b8ef9d67a213c6
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-02-24 12:22:08 -0500

    clean up block class

--- include/frontend/ast/ast_block.hpp
+++ include/frontend/ast/ast_block.hpp
@@ -14,81 +14,79 @@ namespace ast {
  */
 class Block final {
    private:
-    using _block_t = std::vector<StatementUPtr_t>;
-    _block_t block_;
+    using block_t = std::vector<StatementUPtr_t>;
+    block_t block;
 
    public:
-    using value_type = _block_t::value_type;
-    using allocator_type = _block_t::allocator_type;
-    using size_type = _block_t::size_type;
-    using difference_type = _block_t::difference_type;
-    using reference = _block_t::reference;
-    using const_reference = _block_t::const_reference;
-    using pointer = _block_t::pointer;
-    using const_pointer = _block_t::const_pointer;
-    using iterator = _block_t::iterator;
-    using const_iterator = _block_t::const_iterator;
-    using reverse_iterator = _block_t::reverse_iterator;
-    using const_reverse_iterator = _block_t::const_reverse_iterator;
+    using value_type = block_t::value_type;
+    using allocator_type = block_t::allocator_type;
+    using size_type = block_t::size_type;
+    using difference_type = block_t::difference_type;
+    using reference = block_t::reference;
+    using const_reference = block_t::const_reference;
+    using pointer = block_t::pointer;
+    using const_pointer = block_t::const_pointer;
+    using iterator = block_t::iterator;
+    using const_iterator = block_t::const_iterator;
+    using reverse_iterator = block_t::reverse_iterator;
+    using const_reverse_iterator = block_t::const_reverse_iterator;
 
     TypeSPtr_t blockType;
 
    public:
-    constexpr const _block_t& get_block() const noexcept { return block_; }
-    constexpr _block_t& get_block() noexcept { return block_; }
-    
+    constexpr const block_t& get_block() const noexcept { return block; }
+    constexpr block_t& get_block() noexcept { return block; }
+
     // Wrappers around std::vector methods
     // This is partially for backwards compatibility, partially for ease of use
     // Use FORCE_INLINE since these are just wrappers and can very easily be substituted
 
     //~ Element Access
-    constexpr FORCE_INLINE reference operator[](size_type pos) { return block_[pos]; }
-    constexpr FORCE_INLINE const_reference operator[](size_type pos) const { return block_[pos]; }
+    constexpr FORCE_INLINE reference operator[](size_type pos) { return block[pos]; }
+    constexpr FORCE_INLINE const_reference operator[](size_type pos) const { return block[pos]; }
 
-    constexpr FORCE_INLINE reference front() { return block_.front(); }
-    constexpr FORCE_INLINE const_reference front() const { return block_.front(); }
-    constexpr FORCE_INLINE reference back() { return block_.back(); }
-    constexpr FORCE_INLINE const_reference back() const { return block_.back(); }
+    constexpr FORCE_INLINE reference front() { return block.front(); }
+    constexpr FORCE_INLINE const_reference front() const { return block.front(); }
+    constexpr FORCE_INLINE reference back() { return block.back(); }
+    constexpr FORCE_INLINE const_reference back() const { return block.back(); }
 
-    constexpr FORCE_INLINE pointer data() { return block_.data(); }
-    constexpr FORCE_INLINE const_pointer data() const { return block_.data(); }
+    constexpr FORCE_INLINE pointer data() { return block.data(); }
+    constexpr FORCE_INLINE const_pointer data() const { return block.data(); }
 
     //~ Iterators
-    constexpr FORCE_INLINE auto begin() noexcept { return block_.begin(); }
-    constexpr FORCE_INLINE auto begin() const noexcept { return block_.begin(); }
-    constexpr FORCE_INLINE auto cbegin() const noexcept { return block_.cbegin(); }
+    constexpr FORCE_INLINE iterator begin() noexcept { return block.begin(); }
+    constexpr FORCE_INLINE const_iterator begin() const noexcept { return block.begin(); }
+    constexpr FORCE_INLINE const_iterator cbegin() const noexcept { return block.cbegin(); }
 
-    constexpr FORCE_INLINE auto end() noexcept { return block_.end(); }
-    constexpr FORCE_INLINE auto end() const noexcept { return block_.end(); }
-    constexpr FORCE_INLINE auto cend() const noexcept { return block_.cend(); }
+    constexpr FORCE_INLINE iterator end() noexcept { return block.end(); }
+    constexpr FORCE_INLINE const_iterator end() const noexcept { return block.end(); }
+    constexpr FORCE_INLINE const_iterator cend() const noexcept { return block.cend(); }
 
-    constexpr FORCE_INLINE auto rbegin() noexcept { return block_.rbegin(); }
-    constexpr FORCE_INLINE auto rbegin() const noexcept { return block_.rbegin(); }
-    constexpr FORCE_INLINE auto crbegin() const noexcept { return block_.crbegin(); }
+    constexpr FORCE_INLINE reverse_iterator rbegin() noexcept { return block.rbegin(); }
+    constexpr FORCE_INLINE const_reverse_iterator rbegin() const noexcept { return block.rbegin(); }
+    constexpr FORCE_INLINE const_reverse_iterator crbegin() const noexcept { return block.crbegin(); }
 
-    constexpr FORCE_INLINE auto rend() noexcept { return block_.rend(); }
-    constexpr FORCE_INLINE auto rend() const noexcept { return block_.rend(); }
-    constexpr FORCE_INLINE auto crend() const noexcept { return block_.crend(); }
+    constexpr FORCE_INLINE reverse_iterator rend() noexcept { return block.rend(); }
+    constexpr FORCE_INLINE const_reverse_iterator rend() const noexcept { return block.rend(); }
+    constexpr FORCE_INLINE const_reverse_iterator crend() const noexcept { return block.crend(); }
 
     // ~ Capacity
-    constexpr FORCE_INLINE bool empty() const noexcept { return block_.empty(); }
-    constexpr FORCE_INLINE size_type size() const noexcept { return block_.size(); }
-    constexpr FORCE_INLINE size_type max_size() const noexcept { return block_.max_size(); }
-    constexpr FORCE_INLINE void reserve(size_type new_capacity) { block_.reserve(new_capacity); }
-    constexpr FORCE_INLINE size_type capacity() const noexcept { return block_.capacity(); }
-    constexpr FORCE_INLINE void shrink_to_fit() { block_.shrink_to_fit(); }
+    constexpr FORCE_INLINE bool empty() const noexcept { return block.empty(); }
+    constexpr FORCE_INLINE size_type size() const noexcept { return block.size(); }
+    constexpr FORCE_INLINE size_type max_size() const noexcept { return block.max_size(); }
+    constexpr FORCE_INLINE void reserve(size_type new_capacity) { block.reserve(new_capacity); }
+    constexpr FORCE_INLINE size_type capacity() const noexcept { return block.capacity(); }
+    constexpr FORCE_INLINE void shrink_to_fit() { block.shrink_to_fit(); }
 
     //~ Modifiers
-    constexpr FORCE_INLINE void clear() noexcept { block_.clear(); }
+    constexpr FORCE_INLINE void clear() noexcept { block.clear(); }
     constexpr FORCE_INLINE void push_back(const value_type& value) = delete;  // Can't copy a unique_ptr
-    constexpr FORCE_INLINE void push_back(value_type&& value) { block_.push_back(std::move(value)); }
+    constexpr FORCE_INLINE void push_back(value_type&& value) { block.push_back(std::move(value)); }
 
     template <class... Args>
     constexpr FORCE_INLINE reference emplace_back(Args&&... args) {
-        return block_.emplace_back(std::forward(args...));
+        return block.emplace_back(std::forward<Args>(args)...);
     }
-
-    constexpr FORCE_INLINE void pop_back() { block_.pop_back(); }
 };
 }  // namespace ast
 
