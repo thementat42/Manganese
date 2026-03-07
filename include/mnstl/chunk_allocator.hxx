ca68422600f06026bd823b4e760331671940e9fc
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-03-06 22:16:39 -0500

    add chunk allocator class

---
+++ include/mnstl/chunk_allocator.hxx
@@ -0,0 +1,68 @@
+#include <utility>
+#ifndef MNSTL_CHUNK_ALLOCATOR
+#define MNSTL_CHUNK_ALLOCATOR 1
+
+#include <cstddef>
+#include <frontend/ast.hpp>
+#include <memory>
+#include <vector>
+
+#include "frontend/ast/ast_base.hpp"
+
+namespace mnstl {
+
+class chunk_allocator {
+   private:
+    constexpr static inline size_t _chunksize = 4096;
+    struct chunk {
+        std::unique_ptr<std::byte[]> data;
+        size_t used, capacity;
+    };
+    constexpr static inline size_t _max(size_t a, size_t b) noexcept { return a > b ? a : b; }
+
+    std::vector<chunk> _chunks;
+    constexpr static uintptr_t align_up(uintptr_t ptr, uintptr_t alignment) {
+        uintptr_t mask = alignment - 1;
+        return (ptr + mask) & ~mask;
+    }
+
+    void add_chunk(size_t size = _chunksize) {
+        _chunks.push_back(chunk{.data = std::make_unique<std::byte[]>(size), .used = 0, .capacity = size});
+    }
+
+    void* allocate(size_t size, size_t alignment = alignof(std::max_align_t)) {
+    _do_allocation:
+        chunk& c = _chunks.back();
+        uintptr_t current_position = reinterpret_cast<uintptr_t>(c.data.get() + c.used);
+        // the next place we can safely construct a type, taking padding into account
+        uintptr_t aligned_position = align_up(current_position, alignment);
+        uintptr_t adjustment
+            = aligned_position - current_position;  // how much room to leave before the next allocation
+
+        if (c.used + adjustment + size > c.capacity) {
+            // can't fit data here anymore
+            add_chunk(_max(_chunksize, size + alignment));
+            goto _do_allocation;
+        }
+        c.used += adjustment;
+        void* ptr = c.data.get() + c.used;
+        c.used += size;
+
+        return ptr;
+    }
+
+   public:
+    chunk_allocator() { add_chunk(); }
+    ~chunk_allocator() = default;
+
+    template <class Node, class... Args>
+        requires(std::is_convertible_v<Node*, Manganese::ast::ASTNode*> && std::is_constructible_v<Node, Args...>)
+    Node* add_node(Args&&... args) {
+        void* mem = allocate(sizeof(Node), alignof(Node));
+        return new (mem) Node(std::forward<Args>(args)...);
+    }
+};
+
+}  // namespace mnstl
+
+#endif  // MNSTL_CHUNK_ALLOCATOR
