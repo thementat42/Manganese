07acccc23874c9dd026e598b1daa5fd7d5fc7605
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-03-11 00:13:05 -0400

    formatting fix

--- include/frontend/lexer/token.hpp
+++ include/frontend/lexer/token.hpp
@@ -1,15 +1,13 @@
 #ifndef MANGANESE_INCLUDE_FRONTEND_AST_LEXER_TOKEN_HPP
 #define MANGANESE_INCLUDE_FRONTEND_AST_LEXER_TOKEN_HPP
 
-
 /**
  * @file token.hpp
  * @brief Helper include file that includes the different token headers
  */
 
- #include <frontend/lexer/token_type.hpp>
- #include <frontend/lexer/token_base.hpp>
+#include <frontend/lexer/token_base.hpp>
 #include <frontend/lexer/token_helpers.hpp>
+#include <frontend/lexer/token_type.hpp>
 
-
-#endif // MANGANESE_INCLUDE_FRONTEND_AST_LEXER_TOKEN_HPP
+#endif  // MANGANESE_INCLUDE_FRONTEND_AST_LEXER_TOKEN_HPP
