98492f215ea675f61a933b69d214a5eeb67e7af6
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-05-10 21:43:59 -0400

    swap visitor to ast.def

--- include/frontend/ast/visitor_base.hpp
+++ include/frontend/ast/visitor_base.hpp
@@ -20,49 +20,13 @@ class Visitor {
     using typevisit_t = TypeResult;
 
    protected:
-    // Expression Visiting
-    virtual exprvisit_t visit(ast::AggregateInstantiationExpression*) = 0;
-    virtual exprvisit_t visit(ast::AggregateLiteralExpression*) = 0;
-    virtual exprvisit_t visit(ast::ArrayLiteralExpression*) = 0;
-    virtual exprvisit_t visit(ast::AssignmentExpression*) = 0;
-    virtual exprvisit_t visit(ast::BinaryExpression*) = 0;
-    virtual exprvisit_t visit(ast::BoolLiteralExpression*) = 0;
-    virtual exprvisit_t visit(ast::CharLiteralExpression*) = 0;
-    virtual exprvisit_t visit(ast::FunctionCallExpression*) = 0;
-    virtual exprvisit_t visit(ast::GenericExpression*) = 0;
-    virtual exprvisit_t visit(ast::IdentifierExpression*) = 0;
-    virtual exprvisit_t visit(ast::IndexExpression*) = 0;
-    virtual exprvisit_t visit(ast::MemberAccessExpression*) = 0;
-    virtual exprvisit_t visit(ast::NumberLiteralExpression*) = 0;
-    virtual exprvisit_t visit(ast::PostfixExpression*) = 0;
-    virtual exprvisit_t visit(ast::PrefixExpression*) = 0;
-    virtual exprvisit_t visit(ast::ScopeResolutionExpression*) = 0;
-    virtual exprvisit_t visit(ast::StringLiteralExpression*) = 0;
-    virtual exprvisit_t visit(ast::TypeCastExpression*) = 0;
-
-    // Statement Visiting
-    virtual stmtvisit_t visit(ast::AggregateDeclarationStatement*) = 0;
-    virtual stmtvisit_t visit(ast::AliasStatement*) = 0;
-    virtual stmtvisit_t visit(ast::BreakStatement*) = 0;
-    virtual stmtvisit_t visit(ast::ContinueStatement*) = 0;
-    virtual stmtvisit_t visit(ast::EmptyStatement*) = 0;
-    virtual stmtvisit_t visit(ast::EnumDeclarationStatement*) = 0;
-    virtual stmtvisit_t visit(ast::ExpressionStatement*) = 0;
-    virtual stmtvisit_t visit(ast::FunctionDeclarationStatement*) = 0;
-    virtual stmtvisit_t visit(ast::IfStatement*) = 0;
-    virtual stmtvisit_t visit(ast::RepeatLoopStatement*) = 0;
-    virtual stmtvisit_t visit(ast::ReturnStatement*) = 0;
-    virtual stmtvisit_t visit(ast::SwitchStatement*) = 0;
-    virtual stmtvisit_t visit(ast::VariableDeclarationStatement*) = 0;
-    virtual stmtvisit_t visit(ast::WhileLoopStatement*) = 0;
-
-    // Type Visiting
-    virtual typevisit_t visit(ast::AggregateType*) = 0;
-    virtual typevisit_t visit(ast::ArrayType*) = 0;
-    virtual typevisit_t visit(ast::FunctionType*) = 0;
-    virtual typevisit_t visit(ast::GenericType*) = 0;
-    virtual typevisit_t visit(ast::PointerType*) = 0;
-    virtual typevisit_t visit(ast::SymbolType*) = 0;
+#define STMT(name, str) virtual stmtvisit_t visit(ast::name*) = 0;
+#define EXPR(name, str) virtual exprvisit_t visit(ast::name*) = 0;
+#define TYPE(name, str) virtual typevisit_t visit(ast::name*) = 0;
+#include <frontend/ast/ast.def>
+#undef STMT
+#undef EXPR
+#undef TYPE
 
     // Dispatch for the different kinds of nodes
 
@@ -71,7 +35,6 @@ class Visitor {
     typevisit_t visit(ast::Type*);
     // A wrapper around visit(Type*) to handle a shared pointer
     FORCE_INLINE typevisit_t visit(ast::TypeSPtr_t& type) { return visit(type.get()); }
-
 };
 
 }  // namespace ast
