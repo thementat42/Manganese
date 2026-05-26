e6fd55e8fc5a8891056ab8365f8889c51ea1c92d
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-05-25 22:48:46 -0400

    formatting changes, add back equal_to to symbols to allow for heterogenous lookup

--- include/frontend/semantic/symbol_table.hpp
+++ include/frontend/semantic/symbol_table.hpp
@@ -5,6 +5,7 @@
 
 #include <format>
 #include <frontend/ast.hpp>
+#include <functional>
 #include <global_macros.hpp>
 #include <io/logging.hpp>
 #include <string>
@@ -13,7 +14,6 @@
 #include <utility>
 #include <utils/result.hpp>
 #include <vector>
-
 
 namespace Manganese {
 namespace semantic {
@@ -44,11 +44,12 @@ struct Symbol {
 };
 
 struct Scope {
-    // std::equal_to<> enables heterogenous lookup
-    std::unordered_map<std::string_view, Symbol> symbols;
+    // std::equal_to<> enables heterogenous lookup (e.g. looking up with std::string or const char*)
+    // so explicit conversions are not required
+    std::unordered_map<std::string_view, Symbol, std::hash<std::string_view>, std::equal_to<>> symbols;
     Result insert(std::string_view name, Symbol symbol) {
-        bool result = symbols.emplace(name, std::move(symbol)).second;
-        return result ? Result::Success : Result::Failure;
+        bool emplace_succeeded = symbols.emplace(name, std::move(symbol)).second;
+        return emplace_succeeded ? Result::Success : Result::Failure;
     }
 
     const Symbol* lookup(std::string_view name) const noexcept {
@@ -67,24 +68,27 @@ class SymbolTable {
 
    public:
     SymbolTable() noexcept : _scopes(), _currentDepth(0), _hasError(false) { _scopes.emplace_back(); }
-    constexpr ~SymbolTable() = default;
+    constexpr ~SymbolTable() noexcept = default;
 
     constexpr bool hasError() const noexcept { return _hasError; }
 
-    void enterScope() {
+    constexpr void enterScope() {
         ++_currentDepth;
-        // if we've exceeded the current symbol table depth, create a new scope
-        // otherwise, we're just moving our current scope to the next nested one
-        if (size_t(_currentDepth) >= _scopes.size()) { _scopes.emplace_back(); }
+        // if we've exceeded the current symbol table depth, create a new scope (this is on the initial pass)
+        // otherwise, we're just moving our current scope to the next nested one (this is on subsequent passes)
+        if (getCurrentDepth() >= _scopes.size()) { _scopes.emplace_back(); }
     }
+
     void exitScope() noexcept {
         if (noScopeAvailable()) [[unlikely]] {
             logging::logInternal(logging::LogLevel::Warning, "Attempted to exit scope when no scope was available");
             return;
         }
         // since we're doing multiple passes, we want to preserve scope information between passes
+        // so don't use pop_back, since that would destroy the scope and all stored information
         --_currentDepth;
     }
+
     Result declare(std::string_view name, Symbol symbol) {
         if (noScopeAvailable()) [[unlikely]] {
             logging::logInternal(logging::LogLevel::Error, "No active scope in which to declare a symbol");
@@ -95,23 +99,31 @@ class SymbolTable {
     }
 
     const Symbol* lookup(std::string_view name) const noexcept {
-        for (int64_t i = _currentDepth; i >= 0; --i) {  // prioritize local symbols over globals
-            const Symbol* _sym = _scopes[size_t(i)].lookup(name);
-            if (_sym) { return _sym; }
+        // want to prioritize local symbols over globals so iterate backwards
+        // (i.e. from more nested to less nested scopes)
+
+        for (int64_t i = _currentDepth; i >= 0; --i) {
+            const Symbol* _symbol = _scopes[static_cast<size_t>(i)].lookup(name);
+            if (_symbol) { return _symbol; }
         }
         logging::logInternal(logging::LogLevel::Warning, "Symbol '{}' not found in any scope.", name);
         return nullptr;
     }
+
     const Symbol* lookupAtCurrentDepth(std::string_view name) const noexcept {
         if (noScopeAvailable()) {
             logging::logInternal(logging::LogLevel::Error, "No active scope in which to look up symbol");
             return nullptr;
         }
         const Symbol* symbol = _scopes[getCurrentDepth()].lookup(name);
-        if (!symbol) { logging::logInternal(logging::LogLevel::Warning, "Symbol '{}' not found in any scope", name); }
+        if (!symbol) {
+            logging::logInternal(logging::LogLevel::Warning, "Symbol '{}' not found at depth {}", name,
+                                 getCurrentDepth());
+        }
         return symbol;
     }
-    constexpr inline size_t getCurrentDepth() const noexcept { return size_t(_currentDepth); }
+
+    constexpr inline size_t getCurrentDepth() const noexcept { return static_cast<size_t>(_currentDepth); }
 };
 
 }  // namespace semantic
