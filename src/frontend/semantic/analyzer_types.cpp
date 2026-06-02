d8fcd4300e2a43d0aa1ab8a32fe197e73ca6f369
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-06-02 01:23:23 -0400

    add signatures for visiting types (commented out to avoid compiler errors that out-of-line declarations must be definitions)

---
+++ src/frontend/semantic/analyzer_types.cpp
@@ -0,0 +1,16 @@
+#include <frontend/ast.hpp>
+#include <frontend/semantic.hpp>
+#include <core.hpp>
+
+namespace Manganese {
+namespace semantic {
+
+// auto analyzer::visit(ast::AggregateType* type) -> typevisit_t;
+// auto analyzer::visit(ast::ArrayType* type) -> typevisit_t;
+// auto analyzer::visit(ast::FunctionType* type) -> typevisit_t;
+// auto analyzer::visit(ast::GenericType* type) -> typevisit_t;
+// auto analyzer::visit(ast::PointerType* type) -> typevisit_t;
+// auto analyzer::visit(ast::SymbolType* type) -> typevisit_t;
+
+}  // namespace semantic
+}  // namespace Manganese
