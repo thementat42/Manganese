c1898ade270ef4374f40c77d53151f540d6d8c4c
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-07-16 15:15:54 -0400

    add enumMatches function to easily check a value against multiple enum values

---
+++ include/mnstl/enum_matches.hxx
@@ -0,0 +1,16 @@
+#ifndef MNSTL_ENUM_MATCHES
+#define MNSTL_ENUM_MATCHES 1
+
+#include <type_traits>
+
+namespace mnstl {
+
+template <class Enum, class... Args>
+    requires(std::is_enum_v<Enum> && (std::is_same_v<Enum, Args> && ...))
+constexpr bool enumMatches(Enum value, Args&&... args) noexcept {
+    return ((value == args) || ...);
+}
+
+}  // namespace mnstl
+
+#endif  // MNSTL_ENUM_MATCHES
