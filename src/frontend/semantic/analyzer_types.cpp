b87556e2f1077f374729f191848f8e852ed3eea7
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-07-23 16:42:05 -0400

    implement visit functions for types (just calls resolveType)

--- src/frontend/semantic/analyzer_types.cpp
+++ src/frontend/semantic/analyzer_types.cpp
@@ -5,12 +5,24 @@
 namespace Manganese {
 namespace semantic {
 
-// auto analyzer::visit(ast::AggregateType* type) -> typevisit_t;
-// auto analyzer::visit(ast::ArrayType* type) -> typevisit_t;
-// auto analyzer::visit(ast::FunctionType* type) -> typevisit_t;
-// auto analyzer::visit(ast::GenericType* type) -> typevisit_t;
-// auto analyzer::visit(ast::PointerType* type) -> typevisit_t;
-// auto analyzer::visit(ast::SymbolType* type) -> typevisit_t;
+auto analyzer::visit(ast::AggregateType* type) -> typevisit_t {
+    return resolveType(type) ? Result::Success : Result::Failure;
+}
+auto analyzer::visit(ast::ArrayType* type) -> typevisit_t {
+    return resolveType(type) ? Result::Success : Result::Failure;
+}
+auto analyzer::visit(ast::FunctionType* type) -> typevisit_t {
+    return resolveType(type) ? Result::Success : Result::Failure;
+}
+auto analyzer::visit(ast::GenericType* type) -> typevisit_t {
+    return resolveType(type) ? Result::Success : Result::Failure;
+}
+auto analyzer::visit(ast::PointerType* type) -> typevisit_t {
+    return resolveType(type) ? Result::Success : Result::Failure;
+}
+auto analyzer::visit(ast::SymbolType* type) -> typevisit_t {
+    return resolveType(type) ? Result::Success : Result::Failure;
+}
 
 }  // namespace semantic
 }  // namespace Manganese
