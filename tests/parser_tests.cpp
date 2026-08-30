1e08822b86ee08bb982254bab20ea7d51f5c8c6f
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-08-30 18:04:29 -0400

    remove trailing comma in enum parser test

--- tests/parser_tests.cpp
+++ tests/parser_tests.cpp
@@ -446,7 +446,7 @@ static bool testEnumDeclarationStatement() {
     A = 0,
     B,
     C,
-    D,
+    D
 })"};
 
     return validateStatements(getParserResults(expression), expected, "Enum Declaration Statement");
