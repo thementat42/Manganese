7236a7e67d8b1e79d213adafd6c469490ea34312
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-08-29 22:10:47 -0400

    remove constexpr

--- include/mnstl/fold_result.hxx
+++ include/mnstl/fold_result.hxx
@@ -32,55 +32,53 @@ class fold_result_t {
 
    public:
     constexpr fold_result_t() noexcept : _held(held_type::Void) {}
-    constexpr explicit fold_result_t(bool boolean) noexcept : _held(held_type::Boolean), _bool(boolean) {}
-    constexpr explicit fold_result_t(char32_t character) noexcept : _held(held_type::Character), _char(character) {}
-    constexpr explicit fold_result_t(number_t number) noexcept : _held(held_type::Number), _number(number) {}
-    constexpr explicit fold_result_t(std::string_view string) noexcept : _held(held_type::String), _string(string) {}
+    explicit fold_result_t(bool boolean) noexcept : _held(held_type::Boolean), _bool(boolean) {}
+    explicit fold_result_t(char32_t character) noexcept : _held(held_type::Character), _char(character) {}
+    explicit fold_result_t(number_t number) noexcept : _held(held_type::Number), _number(number) {}
+    explicit fold_result_t(std::string_view string) noexcept : _held(held_type::String), _string(string) {}
 
-    constexpr fold_result_t& operator=(bool boolean) noexcept { return *this = fold_result_t(boolean); }
-    constexpr fold_result_t& operator=(char32_t character) noexcept { return *this = fold_result_t(character); }
-    constexpr fold_result_t& operator=(number_t number) noexcept { return *this = fold_result_t(number); }
-    constexpr fold_result_t& operator=(const std::string& string) noexcept {
+    fold_result_t& operator=(bool boolean) noexcept { return *this = fold_result_t(boolean); }
+    fold_result_t& operator=(char32_t character) noexcept { return *this = fold_result_t(character); }
+    fold_result_t& operator=(number_t number) noexcept { return *this = fold_result_t(number); }
+    fold_result_t& operator=(const std::string& string) noexcept {
         return *this = fold_result_t(std::string_view{string});
     }
-    constexpr fold_result_t& operator=(std::string_view string_view) noexcept {
-        return *this = fold_result_t(string_view);
-    }
+    fold_result_t& operator=(std::string_view string_view) noexcept { return *this = fold_result_t(string_view); }
 
-    constexpr fold_result_t(const fold_result_t&) noexcept = default;
-    constexpr fold_result_t& operator=(const fold_result_t&) noexcept = default;
-    constexpr fold_result_t(fold_result_t&&) noexcept = default;
-    constexpr fold_result_t& operator=(fold_result_t&&) noexcept = default;
+    fold_result_t(const fold_result_t&) noexcept = default;
+    fold_result_t& operator=(const fold_result_t&) noexcept = default;
+    fold_result_t(fold_result_t&&) noexcept = default;
+    fold_result_t& operator=(fold_result_t&&) noexcept = default;
 
-    constexpr ~fold_result_t() noexcept = default;
+    ~fold_result_t() noexcept = default;
 
-    constexpr held_type held_type() const noexcept { return _held; }
-    constexpr bool has_value() const noexcept { return _held != held_type::Void; }
-    constexpr bool is_bool() const noexcept { return _held == held_type::Boolean; }
-    constexpr bool is_char() const noexcept { return _held == held_type::Character; }
-    constexpr bool is_number() const noexcept { return _held == held_type::Number; }
-    constexpr bool is_string() const noexcept { return _held == held_type::String; }
+    held_type held_type() const noexcept { return _held; }
+    bool has_value() const noexcept { return _held != held_type::Void; }
+    bool is_bool() const noexcept { return _held == held_type::Boolean; }
+    bool is_char() const noexcept { return _held == held_type::Character; }
+    bool is_number() const noexcept { return _held == held_type::Number; }
+    bool is_string() const noexcept { return _held == held_type::String; }
 
-    constexpr std::optional<bool> boolean() const noexcept {
+    std::optional<bool> boolean() const noexcept {
         return _held == held_type::Boolean ? std::make_optional<bool>(_bool) : std::nullopt;
     }
 
-    constexpr std::optional<char32_t> character() const noexcept {
+    std::optional<char32_t> character() const noexcept {
         return _held == held_type::Character ? std::make_optional<char32_t>(_char) : std::nullopt;
     }
 
-    constexpr std::optional<number_t> number() const noexcept {
+    std::optional<number_t> number() const noexcept {
         return _held == held_type::Number ? std::make_optional<number_t>(_number) : std::nullopt;
     }
 
-    constexpr std::optional<std::string_view> string() const noexcept {
+    std::optional<std::string_view> string() const noexcept {
         return _held == held_type::String ? std::make_optional<std::string_view>(_string) : std::nullopt;
     }
 
     template <class T>
         requires(std::same_as<T, bool> || std::same_as<T, char32_t> || std::same_as<T, number_t>
                  || std::same_as<T, std::string_view>)
-    constexpr std::optional<T> value() const noexcept {
+    std::optional<T> value() const noexcept {
         if constexpr (std::same_as<T, bool>) {
             return boolean();
         } else if constexpr (std::same_as<T, char32_t>) {
@@ -95,10 +93,10 @@ class fold_result_t {
     }
 
     // unchecked functions are faster (no branching) but can be UB
-    constexpr bool boolean_unchecked() const noexcept { return _bool; }
-    constexpr char32_t character_unchecked() const noexcept { return _char; }
-    constexpr number_t number_unchecked() const noexcept { return _number; }
-    constexpr std::string_view string_unchecked() const noexcept { return _string; }
+    bool boolean_unchecked() const noexcept { return _bool; }
+    char32_t character_unchecked() const noexcept { return _char; }
+    number_t number_unchecked() const noexcept { return _number; }
+    std::string_view string_unchecked() const noexcept { return _string; }
 
     template <class T>
         requires(std::same_as<T, bool> || std::same_as<T, char32_t> || std::same_as<T, number_t>
