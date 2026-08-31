7ba3a3ae85fd734e2af43131667473002a4e62c2
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-08-31 17:26:06 -0400

    test runner now always prints the list of passed and failed

--- tests/testrunner.cpp
+++ tests/testrunner.cpp
@@ -26,15 +26,13 @@ void TestRunner::printSummary() const noexcept {
     int total = passed + failed;
     total = total == 0 ? 1 : total;  // avoid any division by 0 problems
     std::cout << ansi::PINK << "\nTest Results:" << ansi::RESET << '\n';
+
+    std::cout << ansi::GREEN << std::format("Tests Passed ({}):\n", passed) << passedTests << "\n";
+    std::cout << ansi::RED << std::format("Tests Failed ({}):\n", failed) << failedTests << ansi::RESET << "\n";
+    if (failed == 0) { std::cout << ansi::GREEN << "All tests passed!" << ansi::RESET << '\n'; }
+
     const float percentPassed = static_cast<float>(passed) / static_cast<float>(total) * 100.0F;
     const float percentFailed = static_cast<float>(failed) / static_cast<float>(total) * 100.0F;
-
-    if (failed > 0) {
-        std::cout << ansi::GREEN << std::format("Tests Passed ({}):\n", passed) << passedTests << "\n";
-        std::cout << ansi::RED << std::format("Tests Failed ({}):\n", failed) << failedTests << ansi::RESET << "\n";
-    } else {
-        std::cout << ansi::GREEN << "All tests passed!" << ansi::RESET << '\n';
-    }
 
     std::cout << "\n----------\n" << ansi::PINK << "Test Summary: " << ansi::RESET << '\n';
     std::cout << ansi::GREEN << std::format("Passed: {}/{} ({:.2f}%)\n", passed, total, percentPassed) << ansi::RESET;
