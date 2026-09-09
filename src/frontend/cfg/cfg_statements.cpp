f3f638c8278e3df11269cdf63f42caa5f1ffc764
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-09-09 16:58:16 -0400

    add handling for blocks

--- src/frontend/cfg/cfg_statements.cpp
+++ src/frontend/cfg/cfg_statements.cpp
@@ -41,9 +41,7 @@ FlowStatus ControlFlowAnalyzer::visit(const ast::ModuleDeclarationStatement* /*u
 
 FlowStatus ControlFlowAnalyzer::visit(const ast::NamespaceStatement* /*unused*/) { return FlowStatus::FallsThrough; }
 
-FlowStatus ControlFlowAnalyzer::visit([[maybe_unused]] const ast::NestedBlockStatement* statement) {
-    return FlowStatus::Returns;
-}
+FlowStatus ControlFlowAnalyzer::visit(const ast::NestedBlockStatement* statement) { return visit(statement->block); }
 
 FlowStatus ControlFlowAnalyzer::visit(const ast::ReturnStatement* /*unused*/) { return FlowStatus::Returns; }
 
@@ -57,6 +55,25 @@ FlowStatus ControlFlowAnalyzer::visit(const ast::VariableDeclarationStatement* /
 
 FlowStatus ControlFlowAnalyzer::visit([[maybe_unused]] const ast::WhileLoopStatement* statement) {
     return FlowStatus::Returns;
+}
+
+FlowStatus ControlFlowAnalyzer::visit([[maybe_unused]] const ast::Block& block) {
+    FlowStatus blockStatus = FlowStatus::FallsThrough;
+    bool isUnreachable = false;
+    for (const ast::Statement* stmt : block) {
+        if (isUnreachable) { logWarning(stmt, "Statement '{}' is unreachable", stmt->toString()); }
+        // if we visit the statement first, we'd get false positives (e.g. a return statement in a function would always
+        // be marked as unreachable)
+        FlowStatus stmtStatus = visit(stmt);
+
+        // if this statement isn't unreachable, we need to update the status
+        // so that subsequent statements can be marked as unreachable
+        if (!isUnreachable) {
+            blockStatus = stmtStatus;
+            isUnreachable = blockStatus != FlowStatus::FallsThrough;
+        }
+    }
+    return blockStatus;
 }
 
 }  // namespace Manganese::cfg
