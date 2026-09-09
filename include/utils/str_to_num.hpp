efd3b417662a5e57432c03e42166cea828971f9a
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-09-09 16:09:19 -0400

    add missing wrap_result call in stringToNumber

--- include/utils/str_to_num.hpp
+++ include/utils/str_to_num.hpp
@@ -134,7 +134,7 @@ inline string_conversion_result_t<T> stringToNumber(std::string_view str, bool i
     if (parsing_end <= base_ptr) { return {.exists = false}; }
 
     if (isFloat) {
-        if (t == float32) { return _stox<mnstl::float32_t>(parsing_start, parsing_end, base); }
+        if (t == float32) { return wrap_result<T>(_stox<mnstl::float32_t>(parsing_start, parsing_end, base)); }
         if (t == not_primitive) {
             auto result32 = _stox<mnstl::float32_t>(parsing_start, parsing_end, base);
             if (!result32.overflowed) { return wrap_result<T>(result32); }
