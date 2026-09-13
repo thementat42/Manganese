fe7e64f1473a9f2d841c87c648c3e58f7732a678
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-09-13 01:43:49 -0400

    add assignment state setting for assignments to variables

--- src/frontend/cfg/control_flow_analyzer_expressions.cpp
+++ src/frontend/cfg/control_flow_analyzer_expressions.cpp
@@ -1,28 +1,34 @@
 #include <frontend/ast.hpp>
 #include <frontend/cfg/control_flow_analyzer.hpp>
-#include "frontend/ast/ast_expressions.hpp"
 
 namespace Manganese::cfg {
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::AggregateInstantiationExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::AggregateInstantiationExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::AggregateLiteralExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::AggregateLiteralExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::AlignofExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::AlignofExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::ArrayLiteralExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::ArrayLiteralExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::AssignmentExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::AssignmentExpression* expression) noexcept {
+    // e.g. assigning an element of an array
+    if (expression->assignee->kind != ast::ExpressionKind::IdentifierExpression) { return; }
+
+    const semantic::Symbol* symbol
+        = symbolTable.lookup(static_cast<const ast::IdentifierExpression*>(expression->assignee)->name);
+    symbolAssignmentStates[symbol->ID] = AssignmentState::Initialized;
+}
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::BinaryExpression* expression) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::BoolLiteralExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::BoolLiteralExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::CharLiteralExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::CharLiteralExpression* /*unused*/) noexcept {}
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::FunctionCallExpression* expression) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::GenericInstantiationExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::GenericInstantiationExpression* /*unused*/) noexcept {}
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::IdentifierExpression* expression) noexcept {}
 
@@ -30,20 +36,20 @@ void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::IndexExpression* exp
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::MemberAccessExpression* expression) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::NumberLiteralExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::NumberLiteralExpression* /*unused*/) noexcept {}
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::PostfixExpression* expression) noexcept {}
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::PrefixExpression* expression) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::ScopeResolutionExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::ScopeResolutionExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::SizeofExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::SizeofExpression* /*unused*/) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::StringLiteralExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::StringLiteralExpression* /*unused*/) noexcept {}
 
 void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::TypeCastExpression* expression) noexcept {}
 
-void ControlFlowAnalyzer::visit([[maybe_unused]] const ast::PoisonedExpression* expression) noexcept {}
+void ControlFlowAnalyzer::visit(const ast::PoisonedExpression* /*unused*/) noexcept {}
 
 }  // namespace Manganese::cfg
