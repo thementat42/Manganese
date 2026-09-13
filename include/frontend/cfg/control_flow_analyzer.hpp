00e14310c27df4ed0a005ccd718488459c0e7d42
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-09-13 18:42:06 -0400

    fix comment in control flow analyzer class

--- include/frontend/cfg/control_flow_analyzer.hpp
+++ include/frontend/cfg/control_flow_analyzer.hpp
@@ -77,7 +77,7 @@ class ControlFlowAnalyzer final : public _flow_base_t {
 #define STMT(name) stmtvisit_t visit(const ast::name*) noexcept override;
 
 #define EXPR(name) exprvisit_t visit(const ast::name*) noexcept override;
-    // Expressions and types don't contribute to control flow or assignment so they can just be no-ops
+    // Types don't contribute to control flow or assignment so they can just be no-ops
 #define TYPE(name) \
     constexpr typevisit_t visit(const ast::name*) noexcept override {}
 
