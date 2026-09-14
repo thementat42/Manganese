6b16c74157b34b33ff0c2ebf6699bcab27bd1685
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-09-13 21:45:05 -0400

    clean up logic for variable declaration control flow visiting

--- src/frontend/cfg/control_flow_analyzer_statements.cpp
+++ src/frontend/cfg/control_flow_analyzer_statements.cpp
@@ -125,11 +125,8 @@ FlowStatus ControlFlowAnalyzer::visit(const ast::SwitchStatement* statement) noe
 FlowStatus ControlFlowAnalyzer::visit(const ast::VariableDeclarationStatement* statement) noexcept {
     const semantic::Symbol* symbol = symbolTable.lookup(statement->name);
     if (symbol != nullptr) {
-        if (statement->value != nullptr) {
-            symbolAssignmentStates[symbol->ID] = AssignmentState::Initialized;
-        } else {
-            symbolAssignmentStates[symbol->ID] = AssignmentState::Uninitialized;
-        }
+        symbolAssignmentStates[symbol->ID]
+            = statement->value == nullptr ? AssignmentState::Uninitialized : AssignmentState::Initialized;
     }
     return FlowStatus::FallsThrough;
 }
