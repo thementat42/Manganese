64f0aa57b43797fe6ed374ac494ba2f48bae7868
thementat42 <187824312+thementat42@users.noreply.github.com>
2026-09-15 22:02:39 -0400

    fix typo

--- src/frontend/ast/ast_to_string.cpp
+++ src/frontend/ast/ast_to_string.cpp
@@ -321,7 +321,7 @@ std::string TypeCastExpression::toString(std::size_t indent) const {
 }
 
 std::string UninitializedExpression::toString(std::size_t /*indent*/) const {
-    return std::format(WRAP("uninitalized"));
+    return std::format(WRAP("uninitialized"));
 }
 
 // Types
